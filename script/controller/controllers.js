f2bd1596ba680069164d358136e3a994d9271f8d
zsedc800 <zsedc800@qq.com>
2016-09-23 11:18:26 +0800

    临时版本

--- script/controller/controllers.js
+++ script/controller/controllers.js
@@ -1,7 +1,15 @@
-app.controller('homeCtrl', function ($scope, $css) {
-	$scope.data = 'home';
+app.controller('homeCtrl', ['$scope', '$css', 'pageLoad', function ($scope, $css, pageLoad) {
+	if (!($scope.titleList = JSON.parse(localStorage.getItem("titleList")))) {
+		$scope.titleList = ['头条', '在现场', '江苏卫视', '荔枝锐评',
+		'原创', '图集', '娱乐', '专题', '荔枝派', '社会', '财经', '体育', '汽车', '科技', '房产',
+		'军事', '监督', '真相帝', '江苏', '国际', '国内'];
+		localStorage.setItem("titleList", JSON.stringify($scope.titleList));
+	}
+	var index = 1;
 	$css.add('css/home.css');
-})
+	pageLoad.getPage('data/2.json', $scope);
+
+}])
 
 .controller('videoCtrl', function ($scope, $css) {
 		$scope.data = 'video';
---
+++ script/factory/factories.js
@@ -0,0 +1,61 @@
+app.factory('pageLoad', ['$http', function ($http) {
+	function getPage(url, scope) {
+		$http.get(url).success(function (res) {
+			setPageData(res, scope);
+		});
+	}
+
+	function setPageData(res, scope) {
+		var list = res.Data.List,
+			i = 0,
+			len = list.length;
+		console.log(list);
+		for (i = 0; i < len; i++) {
+			switch (list[i].Data.MyType) {
+			case '小图+标题+描述':
+				{
+					list[i].Data.Class = 'sm-pic-desc';
+					list[i].myType = 1;
+					break;
+				}
+			case '小图':
+				{
+					list[i].Data.Class = 'sm-pic-desc';
+					list[i].myType = 2;
+					break;
+				}
+			case '大图':
+				{
+					list[i].Data.Class = 'big-imgDiv';
+					list[i].myType = 3;
+					break;
+				}
+			default:
+				break;
+			}
+			if (list[i].Data.HaveVideo === "有视频") {
+				list[i].Data.HaveVideo = true;
+			} else {
+				list[i].Data.HaveVideo = false;
+			}
+		}
+		res.Data.List = list;
+		scope.page1 = res;
+	}
+
+	return {
+		getPage: getPage
+	};
+}]);
+
+
+app.service('pageLoad1', ['$http', function ($http) {
+	function getData(url, scope) {
+		$http.get(url).success(function (res) {
+			console.log(res);
+			scope.page1 = res;
+		});
+	}
+
+	this.getData = getData;
+}]);
